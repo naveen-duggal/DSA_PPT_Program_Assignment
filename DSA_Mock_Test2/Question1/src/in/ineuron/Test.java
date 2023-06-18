1a6667ed1a4368edf52f8476cbbe87866a86ccdb
naveen-duggal <84651021+naveen-duggal@users.noreply.github.com>
2023-06-18 23:40:44 +0530

    Add files via upload

---
+++ DSA_Mock_Test2/Question1/src/in/ineuron/Test.java
@@ -0,0 +1,25 @@
+package in.ineuron;
+
+public class Test {
+
+	public static int mySqrt(int x) {
+	    if (x < 2) {
+	        return x;
+	    }
+
+	    int left = 0;
+	    int right = x;
+
+	    while (left <= right) {
+	        int mid = left + (right - left) / 2;
+	        if (mid > x / mid) {
+	            right = mid - 1;
+	        } else {
+	            left = mid + 1;
+	        }
+	    }
+
+	    return right;
+	}
+
+}
