6a3545183acda7c9f6536af09b11cc7eda46d47e
naveen-duggal <84651021+naveen-duggal@users.noreply.github.com>
2023-07-02 23:35:37 +0530

    Add files via upload

---
+++ Java_PPT_Mock_Test1/VariableDeclaration.java
@@ -0,0 +1,11 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int age = 22;
+        double height = 5.7;
+        String name = "Naveen Duggal";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+    }
+}
